45fe58b2eebc2dd9fec5bbcbac7a82eefc414bb2
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-03-02 22:20:41 -0300

    Exercicio tres

---
+++ Exercicio.Tres/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio.Tres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a distancia percorrida" +
+                "em Km: ");
+            decimal.TryParse(Console.ReadLine(), out decimal kmPercorrido);
+
+            Console.WriteLine("Informe o combustivel gasto na viagem" +
+                "em Litros: ");
+            decimal.TryParse(Console.ReadLine(), out decimal litrosGastos);
+
+            Console.WriteLine($"Sua média foi de {kmPercorrido / litrosGastos}");
+            Console.ReadLine();
+
+        }
+    }
+}
